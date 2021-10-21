54118a9060b87c875285f4e13303aaa6d5889f1a
lflannery <lflannery@seattletimes.com>
2021-10-21 14:57:35 -0700

    now with sliders and bars

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,92 @@
 // var paywall = require("./lib/paywall");
 // setTimeout(() => paywall(12345678), 5000);
+import 'regenerator-runtime/runtime';
+
 require("babel-core/register");
 require("babel-polyfill");
 require("component-responsive-frame/child");
-const d3 = require("d3");
+// const d3 = require("d3");
+import * as d3 from 'd3';
+import { sliderHorizontal } from 'd3-simple-slider';
+
+
+// Hello there, old friend //
+var commafy = s => (s*1).toLocaleString();
+// var commafy = s => (s*1).toLocaleString().replace(/1.0+$/, "");
+
+// All the sliders //
+
+var sliderAge = sliderHorizontal()
+  .min(14)
+  .max(90)
+  .step(1)
+  .width(590)
+  .default(35)
+  .displayValue(false)
+  .on('onchange', (val) => {
+    d3.select('#age').text(val);
+    getValues();
+  });
+
+d3.select('#slider')
+  .append('svg')
+  .attr("preserveAspectRatio", "xMinYMin meet")
+  .attr("viewBox", "0 0 640 70")
+  .append('g')
+  .attr('transform', 'translate(20,30)')
+  .call(sliderAge);
+
+var sliderRetire = sliderHorizontal()
+    .min(30)
+    .max(90)
+    .step(1)
+    .tickValues([30, 40, 50, 60, 70, 80, 90])
+    .width(590)
+    .default(66)
+    .displayValue(false)
+    .on('onchange', (val) => {
+      d3.select('#retire').text(val);
+      getValues();
+    });
+
+  d3.select('#sliderRetire')
+    .append('svg')
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 640 70")
+    // .attr('width', "100%")
+    // .attr('height', 100)
+    .append('g')
+    .attr('transform', 'translate(20,30)')
+    .call(sliderRetire);
+
+var sliderSalary = sliderHorizontal()
+    .min(20000)
+    .max(200000)
+    .step(1000)
+    .width(590)
+    .tickValues([20000, 40000, 60000, 80000, 100000, 120000, 140000, 160000, 180000, 200000])
+    .tickFormat(d3.format("$,~s"))
+    .default(50000)
+    .displayValue(false)
+    .on('onchange', (val) => {
+      d3.select('#salary').text( commafy(val) );
+      getValues();
+    });
+
+
+  d3.select('#sliderSalary')
+    .append('svg')
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 640 70")
+    .append('g')
+    .attr('transform', 'translate(20,30)')
+    .call(sliderSalary);
 
 const taxRate = 0.0058;
-var max = 36500;
+var max = 40000;
+var did_y_change = 40000;
+const benefit2025 = 36500;
+
 
 
 // set the dimensions and margins of the graph
@@ -17,8 +97,10 @@ const margin = {top: 10, right: 30, bottom: 30, left: 60},
 // append the svg object to the body of the page
 const svg = d3.select("#my_dataviz")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+  .attr("preserveAspectRatio", "xMinYMin meet")
+  .attr("viewBox", "0 0 460 400")
+    // .attr("width", width + margin.left + margin.right)
+    // .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform",
           `translate(${margin.left}, ${margin.top})`);
@@ -27,109 +109,105 @@ const svg = d3.select("#my_dataviz")
 // d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv").then( function(data) {
 
   // Add X axis
-  const x = d3.scaleLinear()
-    .domain([2022, 2053])
-    .range([ 0, width ]);
+  // const x = d3.scaleLinear()
+  //   .domain([2022, 2053])
+  //   .range([ 0, width ]);
+
+  const x = d3.scaleBand()
+            .range([0, width])
+            .padding(0.1);
+
+  x.domain(["Lifetime Taxed Amount","State benefit"]);
+
   const xAxis = svg.append("g")
+    .attr("class", "xAxis")
     .attr("transform", `translate(0, ${height})`)
-    .call( d3.axisBottom(x).tickFormat( d3.format("d") ) );
+    .call( d3.axisBottom(x) );
+
+    // .call( d3.axisBottom(x).tickFormat( d3.format("d") ) );
 
 
   // Add Y axis
   const y = d3.scaleLinear()
     .domain([0, 40000])
     .range([ height, 0]);
-  svg.append("g")
+  const yAxis = svg.append("g")
+    .attr("class", "yAxis")
     .call(d3.axisLeft(y).tickFormat( d3.format("$,d") ) );
 
 
 
-
-// Benefits Payout //
     svg.append('g')
-      .append("line")
-        .attr("x1", x(2025) )
-        .attr("y1", y(36500) )
-        .attr("x2", x(2053) )
-        .attr("y2", y(36500) )
-        .style('stroke', 'red')
-        .style('stroke-width', 2);
+    .attr("class", "benefitLine")
+    .append("rect")
+    .attr("x", x("State benefit"))
+    .attr("width", x.bandwidth())
+    .attr("y", y(benefit2025))
+    .attr("height", function(d) { return height - y(benefit2025); })
+    .style('fill', '#99d8c9');
 
-  // start point, end point, vert line, back to origin, back to start point
-  var trianglePointsBenefit = x(2025) + ' ' + y(36500) + ', ' + x(2053) + ' ' + y(36500) + ', ' + x(2053) + ' ' + y(0) + ', ' + x(2025) + ' ' + y(0) + ', ' + x(2025) + ' ' + y(36500);
-
-  // svg.append('g')
-  // .append('polyline')
-  // .attr('points', trianglePointsBenefit)
-  // .style('stroke', 'red')
-  // .style('fill', 'red');
 
   // Lifetime tax cost //
   svg.append('g')
   .attr("class", "taxLine")
-  .append("line")
-    .attr("x1", x(2022) )
-    .attr("y1", y(0) )
-    .attr("x2", x(2053) )
-    .attr("y2", y(8990) )
-    .style('stroke', 'black')
-    .style('color', '#69b3a2');
-
-  // start point, end point, vert line, back to origin, back to start point
-  var trianglePointsTax = x(2022) + ' ' + y(0) + ', ' + x(2053) + ' ' + y(8990) + ', ' + x(2053) + ' ' + y(0) + ', ' + x(2022) + ' ' + y(0);
-
-      svg.append('g')
-      .attr("class", "taxArea")
-      .append('polyline')
-      .attr('points', trianglePointsTax)
-      .style('stroke', 'blue')
-      .style('fill', 'blue');
+  .append("rect")
+  .attr("x", x("Lifetime Taxed Amount"))
+  .attr("width", x.bandwidth())
+  .attr("y", y(8990))
+  .attr("height", function(d) { return height - y(8990); })
+  .style('fill', '#006d2c');
 
 
 
   // grab individual elements to transform later //
   const taxLine = d3.select(".taxLine");
   const taxArea = d3.select(".taxArea");
+  const benLine = d3.select(".benefitLine");
 
   // A function that update the plot for a given xlim value
   function updatePlot(year, endTax) {
+    var rounded = Math.ceil(endTax / 10000) * 10000 // --> 110000
 
-    // Get the value of the button
-    // xlim = this.value
+    // update Y axis if we go over 40K //
+    if ((rounded > did_y_change) || ((rounded < did_y_change) && (rounded > max)) ) {
+      y.domain([0,rounded])
+      yAxis.transition().duration(1000).call( d3.axisLeft(y).tickFormat( d3.format("$,d") ) );
+      did_y_change = rounded;
 
-    // Update X axis
-    x.domain([2022,year])
-    xAxis.transition().duration(1000).call(d3.axisBottom(x))
+      benLine.selectAll("rect")
+         .transition()
+         .duration(1000)
+         .attr("y", y(benefit2025))
+         .attr("height", function(d) { return height - y(benefit2025); });
+
+    } else if ((did_y_change !== max) && (rounded <= max)) {
+      y.domain([0,max])
+      yAxis.transition().duration(1000).call( d3.axisLeft(y).tickFormat( d3.format("$,d") ) );
+      did_y_change = max;
+      // Update Benefit Line
+      benLine.selectAll("rect")
+         .transition()
+         .duration(1000)
+         .attr("y", y(benefit2025))
+         .attr("height", function(d) { return height - y(benefit2025); });
+    } else {}
 
     // Update Tax Line
-    taxLine.selectAll("line")
+    taxLine.selectAll("rect")
        .transition()
        .duration(1000)
-       .attr("x1", x(2022) )
-       .attr("y1", y(0) )
-       .attr("x2", x(year) )
-       .attr("y2", y(endTax) )
+       .attr("y", y(endTax))
+       .attr("height", function(d) { return height - y(endTax); });
 
-  // update Tax Area     - - start point - - -    - - end point - - - - -- - -        - - - vert line -------     - - - back to start - - - -
-  trianglePointsTax = x(2022) + ' ' + y(0) + ', ' + x(year) + ' ' + y(endTax) + ', ' + x(year) + ' ' + y(0) + ', ' + x(2022) + ' ' + y(0);
-
-  taxArea.selectAll("polyline")
-    .transition()
-    .duration(1000)
-    .attr('points', trianglePointsTax)
-    .style('stroke', 'blue')
-    .style('fill', 'blue');
   }
 
 
-  // Add an event listener to the button created in the html part
-  // d3.select("#buttonXlim").on("input", updatePlot )
-
-  document.querySelectorAll(".go").forEach(el => el.addEventListener('click', () => {
-    var age = document.querySelector('#age').value;
-    var retire = document.querySelector('#retire').value;
-    var salary = document.querySelector('#salary').value;
-
+  function getValues() {
+    var age = document.querySelector('#age').innerText;
+    var retire = document.querySelector('#retire').innerText;
+    var salary = document.querySelector('#salary').innerText;
+    // remove comma from salary and convert to int //
+    salary = parseInt(salary.replace(/,/g, ''));
 
     var timespan = retire - age;
     var endYear = 2022 + timespan;
@@ -138,8 +216,15 @@ const svg = d3.select("#my_dataviz")
 
 
     updatePlot(endYear, totalTax);
+    document.querySelector("#lifeTax").innerText = commafy(totalTax.toFixed(2));
+    document.querySelector("#yrTax").innerText = commafy(annualTax.toFixed(2));
+  }
 
-    document.querySelector("#yrTax").innerText = annualTax.toFixed(2);
+
+  // Add an event listener to the button created in the html part
+  // d3.select("#buttonXlim").on("input", updatePlot )
+  document.querySelectorAll(".go").forEach(el => el.addEventListener('click', () => {
+    getValues();
 
   }));
 
